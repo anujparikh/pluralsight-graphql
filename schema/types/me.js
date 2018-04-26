3d3ef1effe80247a390655fbf8dd32fb3f0cc253
212604718 <anuj.parikh@ge.com>
2018-04-26 15:34:46 -0500

    reading data from mongo and postgres db

--- schema/types/me.js
+++ schema/types/me.js
@@ -3,10 +3,12 @@ const {
   GraphQLID,
   GraphQLNonNull,
   GraphQLString,
-  GraphQLList
+  GraphQLList,
+  GraphQLInt
 } = require('graphql');
 
 const pgdb = require('../../database/pgdb');
+const mdb = require('../../database/mdb');
 const ContestType = require('./contest');
 
 module.exports = new GraphQLObjectType({
@@ -27,6 +29,24 @@ module.exports = new GraphQLObjectType({
       type: new GraphQLList(ContestType),
       resolve: (obj, args, {pgPool}) => {
         return pgdb(pgPool).getContest(obj);
+      }
+    },
+    contestsCount: {
+      type: GraphQLInt,
+      resolve: (obj, args, {mPool}, {fieldName}) => {
+        return mdb(mPool).getCounts(obj, fieldName)
+      }
+    },
+    namesCount: {
+      type: GraphQLInt,
+      resolve: (obj, args, {mPool}, {fieldName}) => {
+        return mdb(mPool).getCounts(obj, fieldName)
+      }
+    },
+    votesCount: {
+      type: GraphQLInt,
+      resolve: (obj, args, {mPool}, {fieldName}) => {
+        return mdb(mPool).getCounts(obj, fieldName)
       }
     }
   }
